1a51df51e7aaa343ed90c6a14593ad596fc19ca5
Ayanadiya <ayanadiya17@gmail.com>
2024-10-13 15:24:49 +0530

    Learned to return response based on url of request.

--- app.js
+++ app.js
@@ -3,6 +3,31 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
     console.log("Ayana");
+    const url=req.url;
+    if(url==='/home')
+    {
+        res.write('<html>');
+        res.write('<head><title>My first page</title></head>');
+        res.write('<body><h1>Welcome home</h1></body>');
+        res.write('</html>');
+        return res.end();
+    }
+    if(url==='/about')
+        {
+            res.write('<html>');
+            res.write('<head><title>My first page</title></head>');
+            res.write('<body><h1>Welcome to About Us page</h1></body>');
+            res.write('</html>');
+            return res.end();
+        }
+        if(url==='/node')
+            {
+                res.write('<html>');
+                res.write('<head><title>My first page</title></head>');
+                res.write('<body><h1> Welcome to my Node Js project</h1></body>');
+                res.write('</html>');
+                return res.end();
+            }   
 });
 
 server.listen(4000);
